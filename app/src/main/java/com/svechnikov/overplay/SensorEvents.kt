9f25c55e825d9475bbefe18e36b52ccb99f74ca8
Sergey Svechnikov <svechnikov66@gmail.com>
2021-10-21 14:50:37 +0500

    Connect SensorEvents with rotation detector

--- app/src/main/java/com/svechnikov/overplay/SensorEvents.kt
+++ app/src/main/java/com/svechnikov/overplay/SensorEvents.kt
@@ -5,12 +5,15 @@ import android.content.Context.SENSOR_SERVICE
 import android.hardware.SensorManager
 import android.location.Location
 import com.squareup.seismic.ShakeDetector
+import com.svechnikov.overplay.rotation.AcceleratorMagnetometerRotationDetector
 
 class SensorEvents(context: Context) : ShakeDetector.Listener {
 
     private val sensorManager = context.getSystemService(SENSOR_SERVICE) as SensorManager
 
     private val shakeDetector = ShakeDetector(this)
+
+    private val rotationDetector = AcceleratorMagnetometerRotationDetector()
 
     private lateinit var shakeListener: () -> Unit
 
@@ -25,14 +28,16 @@ class SensorEvents(context: Context) : ShakeDetector.Listener {
     }
 
     fun setRotationListener(listener: (x: Int, y: Int, z: Int) -> Unit) {
-
+        rotationDetector.setListener(listener)
     }
 
     fun start() {
         shakeDetector.start(sensorManager)
+        rotationDetector.start(sensorManager)
     }
 
     fun stop() {
         shakeDetector.stop()
+        rotationDetector.stop()
     }
 }
