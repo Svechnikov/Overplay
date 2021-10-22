27f68dd1f3562033aac4bcd2b0c22ccd5d514c39
Sergey Svechnikov <svechnikov66@gmail.com>
2021-10-22 09:34:48 +0500

    Request location permission

--- app/src/main/java/com/svechnikov/overplay/PlaybackActivity.kt
+++ app/src/main/java/com/svechnikov/overplay/PlaybackActivity.kt
@@ -1,7 +1,11 @@
 package com.svechnikov.overplay
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.StyledPlayerView
 import com.svechnikov.overplay.rotation.GyroscopeRotationDetector
@@ -12,8 +16,25 @@ class PlaybackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        createDefaultGame().start(this)
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { granted ->
+                if (granted) {
+                    startDefaultGame()
+                }
+            }.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            startDefaultGame()
+        }
     }
+
+    private fun startDefaultGame() = createDefaultGame().start(this)
 
     private fun createDefaultGame(): Game {
         val playerView = StyledPlayerView(this).also(::setContentView)
